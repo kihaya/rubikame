91341e3ad90e4084744a0aa5171c0b2b276d8c5e
Kihaya Sugiura <kihayas@gmail.com>
2020-02-08 10:46:23 +0900

    Add Japanese class for double

--- lib/rubikame/kamen_rider_double.rb
+++ lib/rubikame/kamen_rider_double.rb
@@ -3,39 +3,85 @@
 require_relative "../rubikame/kamenriderable"
 
 module Rubikame
+
+  BASIC_INFO = {
+    name:  "仮面ライダーダブルサイクロンジョーカー",
+    height: 195,
+    weight: 85,
+    punch: 2.5,
+    jump: 60,
+    kick: 6,
+    dash: 5.2,
+    year: 2009
+  }
+
   class KamenRiderDouble
     include Rubikame::Kamenriderable
 
     def name
-      "仮面ライダーダブルサイクロンジョーカー"
+      BASIC_INFO[:name]
     end
 
     def height
-      195
+      BASIC_INFO[:height]
     end
 
     def weight
-      85
+      BASIC_INFO[:weight]
     end
 
     def punch
-      2.5
+      BASIC_INFO[:punch]
     end
 
     def jump
-      60
+      BASIC_INFO[:jump]
     end
 
     def kick
-      6
+      BASIC_INFO[:kick]
     end
 
     def dash
-      5.2
+      BASIC_INFO[:dash]
     end
 
     def year
-      2009
+      BASIC_INFO[:year]
+    end
+  end
+
+  class C仮面ライダーダブルサイクロンジョーカー
+    def 名前
+      BASIC_INFO[:name]
+    end
+
+    def 身長
+      BASIC_INFO[:height]
+    end
+
+    def 体重
+      BASIC_INFO[:weight]
+    end
+
+    def パンチ力
+      BASIC_INFO[:punch]
+    end
+
+    def ジャンプ力
+      BASIC_INFO[:jump]
+    end
+
+    def キック力
+      BASIC_INFO[:kick]
+    end
+
+    def 走力
+      BASIC_INFO[:dash]
+    end
+
+    def 放映年
+      BASIC_INFO[:year]
     end
   end
 end
